7d376b501185765a9ef2d71b116364892bf7b93d
IvoCorrea <ivoaurelio30@gmail.com>
2024-01-28 03:18:59 -0300

    Userinfo e readme autalizados

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # coy.js
-My discord bot, made using JavaScript with Discord.js modules.
+
+-- My Discord bot, created using JavaScript with Discord.js modules.
+
+## [Invite me](https://discord.com/api/oauth2/authorize?client_id=1189629794489471006&permissions=8&scope=bot)
+
+# How to use the code?
+
+## Prerequisites
+- You need to install the `dotenv` and `discord.js` libraries.
+
+  ```bash
+  npm install dotenv
+  npm install discord.js
+
+
+## Then you have to create a ".env" file with this information.
+
+- You can obtain these from the Discord Developer Portal.
+TOKEN=00000
+CLIENT_ID=00000
--- comandos/userinfo.js
+++ comandos/userinfo.js
@@ -3,30 +3,32 @@ const { CommandInteraction, EmbedBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-    .setName('userinfo')
-    .setDescription('Diz informações do usuario')
-    .setDescriptionLocalizations({
-      'pt-BR': 'Mostre a sua informação!',
-    })
-   .addUserOption(user => user.setName('user').setDescription('usuario pinto').setRequired(false)),
-   
-    async execute(client , interaction){
-        let user = interaction.options.getUser('user') || interaction.user;
-        //const member = await interaction.guild.members.fetch(user.id);
-        
-        const userInfoEmbed = new EmbedBuilder()
-        .setColor(0x0099FF)
-        .setTitle(`Informações do usuario`)
-        .setDescription(`Informações de ${user}`)
-        .addFields(
-            { name: 'Tag', value: user.tag, inline: true },
-            { name: 'ID', value: user.id, inline: true },
+        .setName('userinfo')
+        .setDescription('Diz informações do usuario')
+        .setDescriptionLocalizations({
+            'pt-BR': 'Mostre a sua informação!',
+        })
+        .addUserOption(user => user.setName('user').setDescription('usuario pinto').setRequired(false)),
 
-        )
-        .setThumbnail(user.displayAvatarURL({ dynamic: true }))
-        .setTimestamp()
-    
+    async execute(client, interaction) {
+        const user = interaction.options.getUser('user') || interaction.user;
+        const member = await interaction.guild.members.fetch(user.id);
+        const isbot = user.bot ? 'Sim' : 'Não';
+        const createdAt = user.createdAt.toLocaleDateString();
+        const joinedAt = member.joinedAt.toLocaleDateString();
+
+        const userInfoEmbed = new EmbedBuilder()
+            .setColor('White')//0x0099FF
+            .setTitle(`**${user.tag}**`)
+            .addFields(
+                { name: 'Tag', value: `**${user.tag}**\`\n${user.id}\``, inline: true },
+                { name: 'Entrou no servidor em', value: `\`${joinedAt}\``, inline: true},
+                { name: 'Data de criação', value: `\`${createdAt}\``,},
+                { name: 'É um bot?', value: `\`${isbot}\``,},   
+            )
+            .setThumbnail(user.displayAvatarURL({ dynamic: true }))
+
         return await interaction.reply({ embeds: [userInfoEmbed] })
-        
+
     }
 };
